36c9755186794df1f5713c0a3bb1d5cf6c048baf
Yuri Winche Achermann <yuri_wa@hotmail.com>
2019-06-08 15:09:37 -0300

    adding EDO python code

---
+++ EDO.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 10:26:41 2019
+
+@author: yuri
+"""
+
+import scipy
+from numpy import pi
+
+
+
+def Drag ( ro , D , Cd , V ):
+    return Cd * ro * V**2 * pi * D**2 / 8
+
+
+
